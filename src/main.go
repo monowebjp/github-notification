ce24d189a5fbc60529ef0cd55e021262895f291d
Nakamura Masumi <capybara@monoweb.jp>
2019-12-23 16:12:08 +0900

    wip

--- src/main.go
+++ src/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-    "bytes"
     "context"
     "fmt"
     "github.com/shurcooL/githubv4"
     "golang.org/x/oauth2"
     "os"
-    "strconv"
     "time"
 )
 
@@ -78,29 +76,70 @@ func main() {
     httpClient := oauth2.NewClient(context.Background(), src)
     client := githubv4.NewClient(httpClient)
 
-    type node struct {
-        Name             githubv4.String
-        DefaultBranchRef struct {
-            Target struct {
-                Commit struct {
-                    History struct {
-                        TotalCount githubv4.Int
-                    } `graphql:"history(since: $t)"`
-                } `graphql:"... on Commit"`
-            }
-        }
-    }
+    //type node struct {
+    //    Name             githubv4.String
+    //    DefaultBranchRef struct {
+    //        Target struct {
+    //            Commit struct {
+    //                History struct {
+    //                    TotalCount githubv4.Int
+    //                } `graphql:"history(since: $t)"`
+    //            } `graphql:"... on Commit"`
+    //        }
+    //    }
+    //}
+    //
+    //var query struct {
+    //    Viewer struct {
+    //        Repositories struct {
+    //            Nodes []node
+    //        } `graphql:"repositories(first: 100)"`
+    //    }
+    //}
+    //
+    //variables := map[string]interface{}{
+    //    "t": githubv4.GitTimestamp(time.Now().Format("2006-01-02T00:00:00+09:00")),
+
+    //{
+    //   user(login: "monowebjp") {
+    //   name
+    //   email
+    //   contributionsCollection(from: "2018-12-23T00:00:00", to: "2019-12-23T23:59:59") {
+    //       totalRepositoryContributions
+    //       totalCommitContributions
+    //       commitContributionsByRepository {
+    //           repository {
+    //               nameWithOwner
+    //           }
+    //           contributions {
+    //               totalCount
+    //           }
+    //       }
+    //   }
+    //}
+    //}
 
     var query struct {
-        Viewer struct {
-            Repositories struct {
-                Nodes []node
-            } `graphql:"repositories(first: 100)"`
-        }
+        User struct {
+            Name                    githubv4.String
+            ContributionsCollection struct {
+                TotalRepositoryContributions githubv4.Int
+                //    TotalCommitContributions        githubv4.Int
+                //    CommitContributionsByRepository []struct {
+                //        Repository struct {
+                //            NameWithOwner githubv4.String
+                //        }
+                //        Contributions struct {
+                //            totalCount githubv4.Int
+                //        }
+                //    }
+            } `graphql:"contributionsCollection(from: $from, to: $to)"`
+        } `graphql:"user(login: \"monowebjp\")"`
     }
 
     variables := map[string]interface{}{
-        "t": githubv4.String(time.Now().Format("2006-01-02T00:00:00+00:00")),
+        "from": githubv4.DateTime(time.Now().Format("2006-01-02T00:00:00")),
+        "to":   githubv4.DateTime(time.Now().Format("2006-01-02T00:00:00")),
     }
 
     err := client.Query(context.Background(), &query, variables)
@@ -108,21 +147,21 @@ func main() {
         fmt.Println(err)
     }
 
-    commitCount := 0
-
-    for _, i := range query.Viewer.Repositories.Nodes {
-        commitCount = commitCount + int(i.DefaultBranchRef.Target.Commit.History.TotalCount)
-    }
+    //commitCount := 0
+    //
+    //for _, i := range query.ContributionsCollection.Repositories.Nodes {
+    //    commitCount = commitCount + int(i.DefaultBranchRef.Target.Commit.History.TotalCount)
+    //}
 
     //api := GetTwitterApi()
 
-    var buffer bytes.Buffer
-    buffer.WriteString("2019年12月20日（火）のコミット数: ")
-    buffer.WriteString(strconv.Itoa(commitCount))
+    //var buffer bytes.Buffer
+    //buffer.WriteString("2019年12月20日（火）のコミット数: ")
+    //buffer.WriteString(commitCount)
     //tweet, err := api.PostTweet(buffer.String(), nil)
     //if err != nil {
     //    panic(err)
     //}
 
-    fmt.Println(buffer.String())
+    fmt.Println(query.User.ContributionsCollection.TotalRepositoryContributions)
 }
